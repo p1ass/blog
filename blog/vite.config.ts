b1c99d93d1c0491f434b7bcc1b5d7c01fac553d7
p1ass <me@p1ass.com>
2024-03-06 23:24:31 +0900

    initialize hono

---
+++ blog/vite.config.ts
@@ -0,0 +1,29 @@
+import pages from "@hono/vite-cloudflare-pages";
+import honox from "honox/vite";
+import client from "honox/vite/client";
+import { defineConfig } from "vite";
+import mdx from "@mdx-js/rollup";
+import remarkFrontmatter from "remark-frontmatter";
+import remarkMdxFrontmatter from "remark-mdx-frontmatter";
+
+export default defineConfig(({ mode }) => {
+  if (mode === "client") {
+    return {
+      plugins: [client()],
+    };
+  } else {
+    return {
+      build: {
+        emptyOutDir: false,
+      },
+      plugins: [
+        honox(),
+        pages(),
+        mdx({
+          jsxImportSource: "hono/jsx",
+          remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
+        }),
+      ],
+    };
+  }
+});
