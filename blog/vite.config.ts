e5517f6bc81cf5df9f4df22de025dacba3d3eea4
p1ass <me@p1ass.com>
2024-03-07 00:38:20 +0900

    ssgプラグインを追加

--- blog/vite.config.ts
+++ blog/vite.config.ts
@@ -1,29 +1,31 @@
-import pages from '@hono/vite-cloudflare-pages'
+import ssg from '@hono/vite-ssg'
+import mdx from '@mdx-js/rollup'
 import honox from 'honox/vite'
 import client from 'honox/vite/client'
-import { defineConfig } from 'vite'
-import mdx from '@mdx-js/rollup'
 import remarkFrontmatter from 'remark-frontmatter'
 import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
+import { defineConfig } from 'vite'
+
+const entry = './app/server.ts'
 
 export default defineConfig(({ mode }) => {
   if (mode === 'client') {
     return {
       plugins: [client()],
     }
-  } else {
-    return {
-      build: {
-        emptyOutDir: false,
-      },
-      plugins: [
-        honox(),
-        pages(),
-        mdx({
-          jsxImportSource: 'hono/jsx',
-          remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
-        }),
-      ],
-    }
+  }
+
+  return {
+    build: {
+      emptyOutDir: false,
+    },
+    plugins: [
+      honox(),
+      ssg({ entry }),
+      mdx({
+        jsxImportSource: 'hono/jsx',
+        remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
+      }),
+    ],
   }
 })
