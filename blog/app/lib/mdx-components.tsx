46f45c64cd5d4b9d94294cc36e2e1f57f362aec7
p1ass <me@p1ass.com>
2024-03-09 21:21:34 +0900

    img tagを調整

--- blog/app/lib/mdx-components.tsx
+++ blog/app/lib/mdx-components.tsx
@@ -1,12 +1,25 @@
+import { css } from 'hono/css'
 import { PropsWithChildren } from 'hono/jsx'
 import { MDXComponents } from 'mdx/types'
 
 export function useMDXComponents(): MDXComponents {
-  const MyH1 = (props: PropsWithChildren) => (
-    <h1 style={{ color: 'tomato' }} {...props} />
-  )
   const components = {
-    h2: MyH1,
+    img: Image,
   }
   return components
+}
+
+const imageCss = css`
+  display: block;
+  max-height: 500px;
+  max-width: 100%;
+  margin: 0 auto;
+`
+
+export function Image(props: PropsWithChildren<Hono.ImgHTMLAttributes>) {
+  return (
+    <a href={props.src}>
+      <img src={props.src} alt={props.alt} class={imageCss} />
+    </a>
+  )
 }
