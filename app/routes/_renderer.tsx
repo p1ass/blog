a2895d5e8953d620e9140fbff28a62db709a348f
p1ass <me@p1ass.com>
2024-04-05 23:25:33 +0900

    og:urlをセット

--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -51,72 +51,78 @@ const mainCss = css`
   max-width: 800px;
 `
 
-export default jsxRenderer(({ children, title: propsTitle, frontmatter }) => {
-  const description =
-    frontmatter?.description ||
-    'Webエンジニアリングについて学んだことや考えたことをまとめるブログです'
+export default jsxRenderer(
+  ({ children, title: propsTitle, frontmatter }, c) => {
+    const description =
+      frontmatter?.description ||
+      'Webエンジニアリングについて学んだことや考えたことをまとめるブログです'
 
-  const title = propsTitle ? `${propsTitle} - ぷらすのブログ` : 'ぷらすのブログ'
-  const ogImage = frontmatter?.title
-    ? `https://og-image.p1ass.com/apiv2/${encodeURIComponent(
-        frontmatter?.title,
-      )}.png`
-    : 'https://blog.p1ass.com/images/ogp.png'
-  return (
-    <html lang='ja'>
-      <head>
-        <meta charset='utf-8' />
-        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-        <title>{title}</title>
+    const title = propsTitle
+      ? `${propsTitle} - ぷらすのブログ`
+      : 'ぷらすのブログ'
+    const ogImage = frontmatter?.title
+      ? `https://og-image.p1ass.com/apiv2/${encodeURIComponent(
+          frontmatter?.title,
+        )}.png`
+      : 'https://blog.p1ass.com/images/ogp.png'
+    return (
+      <html lang='ja'>
+        <head>
+          <meta charset='utf-8' />
+          <meta
+            name='viewport'
+            content='width=device-width, initial-scale=1.0'
+          />
+          <title>{title}</title>
 
-        {/* TODO */}
-        <meta name='description' content={description} />
-        <meta property='og:type' content='website' />
-        <meta property='og:description' content={description} />
-        <meta property='og:image' content={ogImage} />
-        <meta
-          property='og:url'
-          content='https://blog.p1ass.com{{ .Permalink }}'
-        />
-        <meta name='twitter:card' content='summary_large_image' />
-        <meta name='twitter:site' content='@p1ass' />
-        <meta name='twitter:creator' content='@p1ass' />
-        <meta property='og:title' content={title} />
+          <meta name='description' content={description} />
+          <meta property='og:type' content='website' />
+          <meta property='og:description' content={description} />
+          <meta property='og:image' content={ogImage} />
+          <meta
+            property='og:url'
+            content={`https://blog.p1ass.com${c.req.path}`}
+          />
+          <meta name='twitter:card' content='summary_large_image' />
+          <meta name='twitter:site' content='@p1ass' />
+          <meta name='twitter:creator' content='@p1ass' />
+          <meta property='og:title' content={title} />
 
-        <script
-          src='https://kit.fontawesome.com/ea66b8338f.js'
-          crossorigin='anonymous'
-          async
-        />
-        <script
-          async
-          src='https://platform.twitter.com/widgets.js'
-          charset='utf-8'
-        />
+          <script
+            src='https://kit.fontawesome.com/ea66b8338f.js'
+            crossorigin='anonymous'
+            async
+          />
+          <script
+            async
+            src='https://platform.twitter.com/widgets.js'
+            charset='utf-8'
+          />
 
-        <link rel='icon' sizes='48x48' href='/static/favicon.ico' />
-        <link
-          rel='apple-touch-icon'
-          sizes='180x180'
-          href='/static/apple-touch-icon.png'
-        />
-        <link
-          href='/index.xml'
-          rel='alternate'
-          type='application/rss+xml'
-          title='TODO'
-        />
+          <link rel='icon' sizes='48x48' href='/static/favicon.ico' />
+          <link
+            rel='apple-touch-icon'
+            sizes='180x180'
+            href='/static/apple-touch-icon.png'
+          />
+          <link
+            href='/index.xml'
+            rel='alternate'
+            type='application/rss+xml'
+            title='TODO'
+          />
 
-        {/* TODO: Google Absence / Analytics */}
+          {/* TODO: Google Absence / Analytics */}
 
-        <Script src='/app/client.ts' async />
-        <Style />
-      </head>
-      <body class={bodyCss}>
-        <Header />
-        <main class={mainCss}>{children}</main>
-        <Footer />
-      </body>
-    </html>
-  )
-})
+          <Script src='/app/client.ts' async />
+          <Style />
+        </head>
+        <body class={bodyCss}>
+          <Header />
+          <main class={mainCss}>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    )
+  },
+)
