3cd430a419085e597ca69197dd6cf9d6381dc42a
p1ass <me@p1ass.com>
2024-04-06 00:36:18 +0900

    amazonの壊れたリンクを消す

--- app/lib/mdx.ts
+++ app/lib/mdx.ts
@@ -4,10 +4,12 @@ import remarkFrontmatter from 'remark-frontmatter'
 import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
 import type { PluggableList } from 'unified'
 import rehypeMdxImportMedia from 'rehype-mdx-import-media'
+import remarkGfm from 'remark-gfm'
 
 export const remarkPlugins: PluggableList = [
   remarkFrontmatter,
   remarkMdxFrontmatter,
+  remarkGfm,
 ]
 
 export const rehypePlugins: PluggableList = [
